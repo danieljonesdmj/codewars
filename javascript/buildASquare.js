5e615e2b5823d05a3ef56b6031dc17b0599276fb
danieljonesdmj <danieljonesdmj@gmail.com>
2019-06-18 20:01:59 +0100

    Add JS kata

---
+++ javascript/buildASquare.js
@@ -0,0 +1,14 @@
+// Build a square
+// Level 8kyu
+
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 0 and 50.
+
+function generateShape(int){
+  var string = []
+  
+  for (var i = 0; i < int; i++){
+    string.push('+'.repeat(int) + '\n')
+  }
+  
+  return string.join('').slice(0, -1)
+}
